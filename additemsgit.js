87e5d9c86e5ffaa546c732b07162a7a061f458f8
shilpadey <shilpadey735@gmail.com>
2022-08-10 16:24:42 +0530

    tagname

--- additemsgit.js
+++ additemsgit.js
@@ -31,17 +31,31 @@
 //console.log(addItems.innerText);
 
 //GETELEMENTSBYCLASSNAME//
-var items = document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
-items[1].textContent = 'Hello';
-items[1].style.fontWeight = 'bold';
-items[1].style.backgroundColor = 'yellow';
+//var items = document.getElementsByClassName('list-group-item');
+//console.log(items);
+//console.log(items[1]);
+//items[1].textContent = 'Hello';
+//items[1].style.fontWeight = 'bold';
+//items[1].style.backgroundColor = 'yellow';
 
-for(let i = 0; i < items.length; i++) {
-  items[i].style.backgroundColor = '#f4f4f4';
-  items[i].style.fontWeight = 'bold';
+//for(let i = 0; i < items.length; i++) {
+  //items[i].style.backgroundColor = '#f4f4f4';
+  //items[i].style.fontWeight = 'bold';
+//}
+
+//items[2].style.backgroundColor = 'green';
+
+//GETELEMENTBYTAGNAME//
+var li = document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent = 'Hello';
+li[1].style.fontWeight = 'bold';
+li[1].style.backgroundColor = 'yellow';
+
+for(let i = 0; i < li.length; i++) {
+  li[i].style.backgroundColor = '#f4f4f4';
+  li[i].style.fontWeight = 'bold';
 }
 
-items[2].style.backgroundColor = 'green';
-
+li[4].style.backgroundColor = 'green';
