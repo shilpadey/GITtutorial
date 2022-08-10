adf7bf714304f3780d63014a2731c43f33e438d7
shilpadey <shilpadey735@gmail.com>
2022-08-10 13:55:19 +0530

    added colors

--- additemsgit.js
+++ additemsgit.js
@@ -34,3 +34,14 @@
 var items = document.getElementsByClassName('list-group-item');
 console.log(items);
 console.log(items[1]);
+items[1].textContent = 'Hello';
+items[1].style.fontWeight = 'bold';
+items[1].style.backgroundColor = 'yellow';
+
+for(let i = 0; i < items.length; i++) {
+  items[i].style.backgroundColor = '#f4f4f4';
+  items[i].style.fontWeight = 'bold';
+}
+
+items[2].style.backgroundColor = 'green';
+
