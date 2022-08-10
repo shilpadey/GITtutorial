9b314d4a4b363c337fa676b68c8814df8c5fab08
shilpadey <shilpadey735@gmail.com>
2022-08-10 17:22:49 +0530

    queryselector

--- additemsgit.js
+++ additemsgit.js
@@ -46,16 +46,58 @@
 //items[2].style.backgroundColor = 'green';
 
 //GETELEMENTBYTAGNAME//
-var li = document.getElementsByTagName('li');
-console.log(li);
-console.log(li[1]);
-li[1].textContent = 'Hello';
-li[1].style.fontWeight = 'bold';
-li[1].style.backgroundColor = 'yellow';
+//var li = document.getElementsByTagName('li');
+//console.log(li);
+//console.log(li[1]);
+//li[1].textContent = 'Hello';
+//li[1].style.fontWeight = 'bold';
+//li[1].style.backgroundColor = 'yellow';
 
-for(let i = 0; i < li.length; i++) {
-  li[i].style.backgroundColor = '#f4f4f4';
-  li[i].style.fontWeight = 'bold';
+//for(let i = 0; i < li.length; i++) {
+  //li[i].style.backgroundColor = '#f4f4f4';
+  //li[i].style.fontWeight = 'bold';
+//}
+
+//li[4].style.backgroundColor = 'green';
+
+
+//QUERYSELECTOR//
+//var header = document.querySelector('#main-header');
+//header.style.borderBottom = 'solid 4px #ccc';
+
+//var input = document.querySelector('input');
+//input.value = 'Hello World';
+
+//var submit = document.querySelector('input[type="submit"]');
+//submit.value = 'SEND';
+
+//var item = document.querySelector('.list-group-item');
+//item.style.color = 'red';
+
+//var lastItem = document.querySelector('.list-group-item:last-child');
+//lastItem.style.color = 'blue';
+
+//var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+//secondItem.style.color = 'coral';
+//secondItem.style.backgroundColor = 'green';
+
+//var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+//thirdItem.style.color = 'white';
+
+//QUERYSELECTORALL//
+var titles = document.querySelectorAll('.title');
+console.log(titles);
+titles[0].textContent = 'Hello';
+
+var item = document.querySelectorAll('.list-group-item');
+console.log(item);
+item[1].style.color = 'green';
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+var even = document.querySelectorAll('li:nth-child(even)');
+
+
+for(let i = 0; i < odd.length; i++) {
+  odd[i].style.backgroundColor = 'green';
+  even[i].style.backgroundColor = '#f4f4f4';
 }
-
-li[4].style.backgroundColor = 'green';
